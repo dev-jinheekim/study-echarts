749a372ed9617f65401eab2225f80b3a9e7bc9dc
Jinhee Kim <jinhee.kim@solarconnect.kr>
2020-11-27 19:49:23 +0900

    전력사용량통계 : 3D차트 추가

--- main.js
+++ main.js
@@ -3,16 +3,19 @@ import {option as powerUsageStatusOption} from '/chart/power-usage-status.js'
 import {option as reductionResultOption} from '/chart/reduction-result.js'
 import {option as instantaneousValueOption} from '/chart/instantaneous-value.js'
 import {option as powerUsageStatistics2dOption} from '/chart/power-usage-statistics-2d.js'
+import {option as powerUsageStatistics3dOption} from '/chart/power-usage-statistics-3d.js'
 import {option as powerUsageStatisticsVisualizationAnalysisOption} from '/chart/power-usage-statistics-visualization-analysis'
 
 const powerUsageStatus = echarts.init(document.getElementById('power-usage-status'));
 const reductionResult = echarts.init(document.getElementById('reduction-result'));
 const instantaneousValue = echarts.init(document.getElementById('instantaneous-value'));
 const powerUsageStatistics2d = echarts.init(document.getElementById('power-usage-statistics-2d'));
+const powerUsageStatistics3d = echarts.init(document.getElementById('power-usage-statistics-3d'));
 const powerUsageStatisticsVisualizationAnalysis = echarts.init(document.getElementById('power-usage-statistics-visualization-analysis'));
 
 powerUsageStatus.setOption(powerUsageStatusOption);
 reductionResult.setOption(reductionResultOption);
 instantaneousValue.setOption(instantaneousValueOption);
 powerUsageStatistics2d.setOption(powerUsageStatistics2dOption);
+powerUsageStatistics3d.setOption(powerUsageStatistics3dOption);
 powerUsageStatisticsVisualizationAnalysis.setOption(powerUsageStatisticsVisualizationAnalysisOption);
